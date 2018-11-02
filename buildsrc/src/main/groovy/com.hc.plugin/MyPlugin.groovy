27732d480225acd773dc4f1a45a63c9ab4c89376
chenxiaokai <chenxiaokai@finupgroup.com>
2018-11-02 14:39:25 +0800

    增加代码 来查看打印效果

--- buildsrc/src/main/groovy/com.hc.plugin/MyPlugin.groovy
+++ buildsrc/src/main/groovy/com.hc.plugin/MyPlugin.groovy
@@ -12,6 +12,48 @@ public class MyPlugin implements Plugin<Project> {
         //-------------------> dir = E:\github\HCPlugin
         println "-------------------> dir = "+project.getRootDir().absolutePath
 
+
+        /*
+---------------> variant = QQDebug baseName = QQ-debug des= DebugQQ build appId()= com.hc.hcplugin flavorName =QQ variantData = ApplicationVariantData{QQDebug} scope = com.android.build.gradle.internal.scope.VariantScopeImpl@133b8413
+---------------> variant = QQRelease baseName = QQ-release des= ReleaseQQ build appId()= com.hc.hcplugin flavorName =QQ variantData = ApplicationVariantData{QQRelease} scope = com.android.build.gradle.internal.scope.VariantScopeImpl@48640abb
+---------------> variant = baiduDebug baseName = baidu-debug des= DebugBaidu build appId()= com.hc.hcplugin flavorName =baidu variantData = ApplicationVariantData{baiduDebug} scope = com.android.build.gradle.internal.scope.VariantScopeImpl@148ad4d
+---------------> variant = baiduRelease baseName = baidu-release des= ReleaseBaidu build appId()= com.hc.hcplugin flavorName =baidu variantData = ApplicationVariantData{baiduRelease} scope = com.android.build.gradle.internal.scope.VariantScopeImpl@51793112
+         */
+        android.applicationVariants.all { variant ->
+            println("---------------> variant = "+variant.getName()+" baseName = "+variant.getBaseName() +" des= " + variant.getDescription()
+            +" appId()= "+variant.getApplicationId() +" flavorName =" + variant.getFlavorName()
+            +" variantData = "+variant.variantData +" scope = "+variant.variantData.scope
+            +" appPackageName = "+variant.generateBuildConfig.appPackageName)
+
+            /*
+            ---------------> path = /Users/chenxiaokai/AndroidStudioProjects/HCPlugin/app/build/generated/source/buildConfig/QQ/debug
+            ---------------> path = /Users/chenxiaokai/AndroidStudioProjects/HCPlugin/app/build/generated/source/buildConfig/QQ/release
+            ---------------> path = /Users/chenxiaokai/AndroidStudioProjects/HCPlugin/app/build/generated/source/buildConfig/baidu/debug
+            ---------------> path = /Users/chenxiaokai/AndroidStudioProjects/HCPlugin/app/build/generated/source/buildConfig/baidu/release
+             */
+            println("---------------> path = "+ variant.getVariantData().getScope().getBuildConfigSourceOutputDir())
+
+            /*
+            ---------------> configName = generateQQDebugBuildConfig
+            ---------------> configName = generateQQReleaseBuildConfig
+            ---------------> configName = generateBaiduDebugBuildConfig
+            ---------------> configName = generateBaiduReleaseBuildConfig
+             */
+            println("---------------> configName = "+ variant.getVariantData().getScope().getGenerateBuildConfigTask().name)
+
+            /*
+            ---------------> MergeAssetName = mergeQQDebugAssets
+            ---------------> MergeAssetName = mergeQQReleaseAssets
+            ---------------> MergeAssetName = mergeBaiduDebugAssets
+            ---------------> MergeAssetName = mergeBaiduReleaseAssets
+             */
+            println("---------------> MergeAssetName = "+ variant.getVariantData().getScope().getMergeAssetsTask().name)
+
+            def mergeAssetsTask = project.tasks.getByName(variant.getVariantData().getScope().getMergeAssetsTask().name)
+
+            println("----------------> filtDir = "+mergeAssetsTask.outputDir)
+        }
+
         android.registerTransform(new MyTransform(project))
     }
 }
